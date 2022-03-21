e3e1a5228b5b922117e46d2ba5d6de12056ab837
Evgeny Mironov <e.d.mironov@yandex.ru>
2022-03-21 20:26:54 +0300

    added test

--- src/test/java/ru/netology/ApiTest.java
+++ src/test/java/ru/netology/ApiTest.java
@@ -10,15 +10,14 @@ public class ApiTest {
     void shouldReturnDemoAccounts() {
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("my head hurts") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("my head hurts"))
         ;
-
     }
 }
